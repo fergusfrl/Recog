4fbb001861e97bc64c86758ac5fcbec24012869d
Fergus Farrell <fergusf@orionhealth.com>
2019-06-06 15:27:01 +1200

    some changes to improve codacy compliance

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,27 +1,27 @@
-let path = require('path');
-let exec = require('child_process').exec;
+const path = require('path');
+const exec = require('child_process').exec;
 import * as fs from 'fs';
 
 test('Should create component', async () => {
-    let result: any = await CLI([]);
+    const result: any = await CLI([]);
     expect(result.code).toBe(0);
     expect(fs.existsSync('./TestComponent.jsx')).toBe(true);
 });
 
 test('Should create typescript component', async () => {
-    let result: any = await CLI(['-t']);
+    const result: any = await CLI(['-t']);
     expect(result.code).toBe(0);
     expect(fs.existsSync('./TestComponent.tsx')).toBe(true);
 });
 
 test('Should create stateful component', async () => {
-    let result: any = await CLI(['-s']);
+    const result: any = await CLI(['-s']);
     expect(result.code).toBe(0);
     expect(fs.existsSync('./TestComponent.jsx')).toBe(true);
 });
 
 test('Should create stateful typescript component', async () => {
-    let result: any = await CLI(['-t -s']);
+    const result: any = await CLI(['-t -s']);
     expect(result.code).toBe(0);
     expect(fs.existsSync('./TestComponent.tsx')).toBe(true);
 });
@@ -32,10 +32,10 @@ const CLI = (args: string[]) => {
             `node ${path.resolve('./dist/src/index.js')} new TestComponent ./ ${args.join(' ')}`,
             './tests',
             (error: any, stdout: string, stderr: string) => resolve({
-                code: error && error.code ? error.code : 0,
                 error,
                 stdout,
-                stderr
+                stderr,
+                code: error && error.code ? error.code : 0
             })
         );
     });
